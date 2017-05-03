39e302a6be0e17ad62d2ac54731ff772d9ecb0ca
Ondrej Kosarko <ko_ok@centrum.cz>
2017-05-03 17:11:29 +0200

    travis, pylint config, requirements-dev, suggestions by pylint on org.shortref

--- org/shortref.py
+++ org/shortref.py
@@ -1,6 +1,6 @@
-import requests
 import logging
 from pprint import pformat
+import requests
 
 
 class Shortref(object):
@@ -10,13 +10,13 @@ class Shortref(object):
     @staticmethod
     def obtain_handle(target_url, title, reportemail, subprefix):
         """register the target_url with shortref and return the handle"""
-        r = requests.post(Shortref.SHORTREF_API_URL,
-                          json={'url': target_url, 'reportemail': reportemail,
-                                'title': title, 'subprefix': subprefix})
-        logging.debug(pformat(r))
-        r.raise_for_status()
-        shortref_object = r.json()
-        logging.info('Successfully created handle "{}" for "{}"'.format(shortref_object[
-                                                                            'handle'], target_url))
+        response = requests.post(Shortref.SHORTREF_API_URL,
+                                 json={'url': target_url, 'reportemail': reportemail,
+                                       'title': title, 'subprefix': subprefix})
+        logging.debug(pformat(response))
+        response.raise_for_status()
+        shortref_object = response.json()
+        logging.info('Successfully created handle "%s" for "%s"', shortref_object['handle'],
+                     target_url)
         logging.debug(pformat(shortref_object))
         return shortref_object['handle']
