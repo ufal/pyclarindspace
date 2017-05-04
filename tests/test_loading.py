4ebffe13e419561727e1944998bafb29ffe9130c
jm <jm@server>
2017-05-04 11:20:03 +0200

    fixing 3.6

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -7,7 +7,7 @@ class Testloading(unittest.TestCase):
     def test_import(self):
         """ test_import """
         import clarindspace
-        print clarindspace.__version__
+        print(clarindspace.__version__)
 
 if __name__ == '__main__':
     unittest.main()
