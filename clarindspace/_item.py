fcf6ae1068fdf1a35d19e3ef7c45c523c2644234
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2017-06-13 15:37:13 +0200

    create new version for item

--- clarindspace/_item.py
+++ clarindspace/_item.py
@@ -12,12 +12,13 @@ class item(object):
         DSpace item representation. Holds name, id & handle; methods to add bitstreams
     """
 
-    def __init__(self, name, item_id, handle, repository=None):
+    def __init__(self, name, item_id, handle, owning_collection, repository=None):
         """Constructor for Item"""
         self._name = name
         self._id = item_id
         self._handle = handle
         self._repository = repository
+        self._owning_collection = owning_collection
 
     @staticmethod
     def bitstream_info_from_pid(pid_url, mimetype=None):
@@ -102,6 +103,12 @@ class item(object):
             '/items/' + str(self._id) + '/metadata', json_metadata_entry_array)
         logging.info('Successfully replaced metadata on item [%s].', self._id)
 
+    def add_metadata(self, json_metadata_entry_array):
+        """Add metadata entries"""
+        self._repository.api_post(
+            '/items/' + str(self._id) + '/metadata', json_metadata_entry_array)
+        logging.info('Successfully added metadata to item [%s].', self._id)
+
     def update_identifier(self, handle):
         """
             Replace all dc.identifier.uri with the supplied handle
@@ -117,3 +124,29 @@ class item(object):
     def get_metadata(self):
         url = '/items/' + str(self._id) + '/metadata'
         return self._repository.api_get(url)
+
+    def create_new_version(self):
+        """Creates a new item as a version of this"""
+        metadata = self.get_metadata()
+        # will reuse the metadata (cleanup might needed)
+        # see https://github.com/ufal/clarin-dspace/blob/clarin/dspace-xmlui/src/main/java/cz/cuni/mff/ufal/dspace/app/xmlui/aspect/submission/submit/AddNewVersionAction.java
+        omit_keys = ('dc.identifier.uri', 'dc.date.accessioned', 'dc.date.available', 'dc.description.provenance',
+                     'local.featuredService', 'dc.relation.replaces', 'dc.relation.isreplacedby')
+        cleaned_up_metadata = ()
+        for obj in metadata:
+            if obj['key'] == 'dc.title':
+                obj['value'] += ' v2.0'
+            elif obj['key'] == 'local.submission.note':
+                obj['value'] = 'Thise item is a new version of ' + self.handle
+            elif obj['key'] in omit_keys:
+                continue
+            cleaned_up_metadata.append(obj)
+
+
+        # prepare replaces field
+        cleaned_up_metadata.append({'key': 'dc.relation.replaces', 'value': 'http://hdl.handle.net/' + self.handle})
+        new_version = self._owning_collection.create_item(cleaned_up_metadata)
+        # update self with isreplacedby
+        self.add_metadata([{'key': 'dc.relation.isreplacedby', 'value': 'http://hdl.handle.net/' + new_version.handle}])
+        return new_version
+
